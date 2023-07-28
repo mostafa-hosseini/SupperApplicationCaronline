68f2a4705e1715d91ea6db543a7804685337e54b
Mahdi Zare <mzare0930@gmail.com>
2023-07-28 19:35:33 +0430

    redesign login and register

--- SuperApp.Caronlineofficial.com/ClientApp/src/Screens/Login.js
+++ SuperApp.Caronlineofficial.com/ClientApp/src/Screens/Login.js
@@ -1,44 +1,41 @@
 import React from "react";
-import { FaUser, FaUserLock, FaUserPlus } from "react-icons/fa";
+import logo from "../assets/images/Icons/logo/kalato-01.png";
 
 export default function Login() {
   return (
-    <div className="row " style={{ height: "90vh" }}>
-      <div
-        className="col-10 col-md-8 mx-auto row"
-        style={{ maxHeight: "35vh" }}
-      >
-        <div className="w-100 p-4 back-ground shadow mt-5 row">
-          <div className="col-auto mx-auto">
-            <div className="icon-aut ">
-              <FaUserLock size={50} color="#000" />
-            </div>
-          </div>
-          <div className="w-100"></div>
+    <div className="login-wrapper">
+      <div className="login-box">
+        <div className="login-img">
+          <img src={logo} alt="car online logo" />
+        </div>
 
-          <div className="col-12 mt-3">
-            <h3 className="text-center mb-2">ورود به حساب کاربری</h3>
-          
-            <input
-              type="text"
-              name="phoneNumber"
-              className="form-control back-ground"
-              placeholder=" شماره موبایل خودرا وارد نمایید"
-              id=""
-            />
-          </div>
-          <div className="col-12 mt-3">
-            <input
-              type="text"
-              name="password"
-              className="form-control back-ground"
-              placeholder="کلمه عبور را وارد نمایید"
-              id=""
-            />
-          </div>
-          <div className="col-12 mt-3">
-            <div className="btn btn-success btn-block w-100 p-2">ورود</div>
-          </div>
+        <div className="login-text w-100 mb-2">
+          <h3 className="title">ورود / ثبت نام</h3>
+          <h4 className="desc">لطفا شماره موبایل یا ایمیل خود را وارد کنید.</h4>
+        </div>
+
+        <input
+          type="text"
+          name="phoneNumber"
+          className="form-control back-ground"
+          placeholder=" شماره موبایل یا ایمیل خود را وارد نمایید"
+          id=""
+        />
+        {/* <input
+          type="text"
+          name="password"
+          className="form-control back-ground"
+          placeholder="کلمه عبور را وارد نمایید"
+          id=""
+        /> */}
+        <div className="w-100 mt-4">
+          <div className="btn btn-login btn-block w-100 p-2">ورود</div>
+        </div>
+
+        <div className="notes">
+          <span>
+            ورود شما به معنای پذیرش شرایط کارآنلاین و قوانین و حریم خصوصی است.
+          </span>
         </div>
       </div>
     </div>
--- SuperApp.Caronlineofficial.com/ClientApp/src/Screens/Register.js
+++ SuperApp.Caronlineofficial.com/ClientApp/src/Screens/Register.js
@@ -1,49 +1,65 @@
 import React, { useState } from "react";
-import { FaUserPlus } from "react-icons/fa";
-import http from '../api/index';
+import logo from "../assets/images/Icons/logo/kalato-01.png";
+import http from "../api/index";
 import { useNavigate } from "react-router-dom";
 
-export default function Register() {
-  const [input, setinput] = useState({ phoneNumber: "" })
+export default function Login() {
+  const [input, setinput] = useState({ phoneNumber: "" });
   const api = http();
   const navigate = useNavigate();
-  return (
-    <div className="row " style={{ height: "90vh" }}>
-      <div className="col-10 col-md-8 mx-auto row" style={{ maxHeight: "35vh" }}>
-        <div className="w-100 p-4 back-ground mt-5 row">
-          <div className="col-auto mx-auto">
-            <div className="icon-aut ">
-              <FaUserPlus size={50} color="#000" />
-            </div>
-          </div>
-          <div className="w-100"></div>
 
-          <div className="col-12 mt-3">
-            <h3 className="text-center mb-2">
-              ثبت نام
-            </h3>
-            <h6 className="text-center">
-              برای ثبت نام شماره موبایل خودرا وارد نمایید
-            </h6>
-            <input
-              type="text"
-              name="phoneNumber"
-              className="form-control back-ground"
-              placeholder="09120000000"
-              id=""
-              onChange={(e) => setinput({ ...input, phoneNumber: e.target.value })}
-              value={input.phoneNumber}
-            />
+  return (
+    <div className="login-wrapper">
+      <div className="login-box">
+        <div className="login-img">
+          <img src={logo} alt="car online logo" />
+        </div>
+
+        <div className="login-text w-100 mb-2">
+          <h3 className="title">ورود / ثبت نام</h3>
+          <h4 className="desc">کد دریافتی خود را وارد کنید.</h4>
+        </div>
+
+        <input
+          type="text"
+          name="phoneNumber"
+          className="form-control back-ground"
+          placeholder="کد خود را وارد کنید."
+          id=""
+          onChange={(e) => setinput({ ...input, phoneNumber: e.target.value })}
+          value={input.phoneNumber}
+        />
+        {/* <input
+          type="text"
+          name="password"
+          className="form-control back-ground"
+          placeholder="کلمه عبور را وارد نمایید"
+          id=""
+        /> */}
+        <div className="w-100 mt-4">
+          <div
+            className="btn btn-login btn-block w-100 p-2"
+            onClick={() => {
+              api.Register(
+                ({ data }) => {
+                  if (data.isSuccess) {
+                    navigate("/verify", {
+                      state: { phoneNumber: input.phoneNumber },
+                    });
+                  }
+                },
+                { phone: input.phoneNumber }
+              );
+            }}
+          >
+            ورود
           </div>
-          <div className="col-12 mt-3">
-            <div onClick={() => {
-              api.Register(({ data }) => {
-                if (data.isSuccess) {
-                  navigate("/verify", { state: { phoneNumber: input.phoneNumber } })
-                }
-              }, { phone: input.phoneNumber })
-            }} className="btn btn-success btn-block w-100 p-2">دریافت کد</div>
-          </div>
+        </div>
+
+        <div className="notes">
+          <span>
+            ورود شما به معنای پذیرش شرایط کارآنلاین و قوانین و حریم خصوصی است.
+          </span>
         </div>
       </div>
     </div>
